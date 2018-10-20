c9649b27d3a39bcef46c891240aeed5ba44ebdf3
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-20 11:00:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-  this.veto = function(){
-    console.log("No, I must disagree")
-  }
+  this.veto = () => ("No, I must disagree")
   this.approve = () => ("You can do that!")
+  this.doCharity = () => ("I like to help people.")
 }
