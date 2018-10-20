aafd69cc81e99ab019d4b3360286931ec897a32a
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-20 11:07:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function BoardMember(name, homeState, training){
   this.veto = () => ("No, I must disagree")
   this.approve = () => ("You can do that!")
   this.doCharity = () => ("I like to help people.")
-  this.releasePressStatement = () => ("You will see great things from Scuber.")
+  this.releasePressStatement = function(){
+    return ("You will see great things from Scuber.")
+   }
   this.sayHi = () => (`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`)
 }
